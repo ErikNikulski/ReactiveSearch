9e6cf99ddc81ccc992374218957848e583b08299
Erik Nikulski <erik.nikulski@mailbox.tu-dresden.de>
2019-01-20 09:50:32 +0100

    Change tab indentation from 2 spaces to 4 spaces

--- src/App.js
+++ src/App.js
@@ -4,56 +4,56 @@ import $ from 'jquery';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      options: []
+        this.state = {
+            options: []
+        }
     }
-  }
 
-  changeStructure(array) {
-    // https://stackoverflow.com/a/29177205
-    return array.reduce(function(acc, obj) {
-      Object.keys(obj).forEach(function(k) {
-        acc[k] = (acc[k] || []).concat(obj[k])
-      });
-      return acc
-    },{})
-  }
+    changeStructure(array) {
+        // https://stackoverflow.com/a/29177205
+        return array.reduce(function(acc, obj) {
+            Object.keys(obj).forEach(function(k) {
+                acc[k] = (acc[k] || []).concat(obj[k])
+            });
+            return acc
+        },{})
+    }
 
-  getData() {
-    $.ajax({
-      url: 'https://jsonplaceholder.typicode.com/comments',
-      dataType: 'json',
-      cache: false,
-      success: function (data) {
-        let options = this.changeStructure(data);
-        delete options.id;
-        delete options.postId;
-        this.setState({options: options})
-      }.bind(this),
-      error: function (xhr, status, err) {
-        console.log(err);
-      }
-    })
-  }
+    getData() {
+        $.ajax({
+            url: 'https://jsonplaceholder.typicode.com/comments',
+            dataType: 'json',
+            cache: false,
+            success: function (data) {
+                let options = this.changeStructure(data);
+                delete options.id;
+                delete options.postId;
+                this.setState({options: options})
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.log(err);
+            }
+        })
+    }
 
-  componentWillMount() {
-    this.getData();
-  }
+    componentWillMount() {
+        this.getData();
+    }
 
-  componentDidMount() {
-    this.getData();
-  }
+    componentDidMount() {
+        this.getData();
+    }
 
-  render() {
-    return (
-      <div className='App'>
-        <Search data={this.state.options}/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='App'>
+                <Search data={this.state.options}/>
+            </div>
+        );
+    }
 }
 
 export default App;
